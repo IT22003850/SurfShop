26e7d6d7482aec7c5914c7ac527474b1b611ce4e
Thilina Thushara <thilina.sam.a7@gmail.com>
2024-12-05 13:22:24 +0530

    init

---
+++ src/test/java/com/paradisemidigama/SurfShop/SurfShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradisemidigama.SurfShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
