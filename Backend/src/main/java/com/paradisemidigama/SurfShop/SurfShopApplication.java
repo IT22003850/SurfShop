478a695b7d828496b6b19bb0ab0ded8006057c6d
Thilina Thushara <thilina.sam.a7@gmail.com>
2024-12-06 16:55:02 +0530

    basic front-end

---
+++ Backend/src/main/java/com/paradisemidigama/SurfShop/SurfShopApplication.java
@@ -0,0 +1,13 @@
+package com.paradisemidigama.SurfShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfShopApplication.class, args);
+	}
+
+}
